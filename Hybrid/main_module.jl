075443a8ee704ed4456b2d35d5355b247fd4fdaf
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-09-06 11:53:29 +0530

    Another safety commit

--- Hybrid/main_module.jl
+++ Hybrid/main_module.jl
@@ -108,11 +108,11 @@ include("file.jl")
 # export returnFileLength, readFile
 
 include("flux_residual.jl")
-# include("flux_residual_cuda.jl")
+include("flux_residual_cuda.jl")
 # export cal_flux_residual, wallindices_flux_residual, outerindices_flux_residual, interiorindices_flux_residual
 
 include("interior_fluxes.jl")
-# include("interior_fluxes_cuda.jl")
+include("interior_fluxes_cuda.jl")
 # export interior_dGx_pos, interior_dGx_neg, interior_dGy_pos, interior_dGy_neg
 
 include("limiters.jl")
@@ -125,25 +125,25 @@ include("objective_function.jl")
 # export calculateTheta, compute_cl_cd_cm
 
 include("outer_fluxes.jl")
-# include("outer_fluxes_cuda.jl")
+include("outer_fluxes_cuda.jl")
 # export outer_dGx_pos, outer_dGx_neg, outer_dGy_pos
 
 include("point.jl")
 # export getxy, setConnectivity
 
 include("quadrant_fluxes.jl")
-# include("quadrant_fluxes_cuda.jl")
+include("quadrant_fluxes_cuda.jl")
 # export flux_quad_GxI, flux_quad_GxII, flux_quad_GxIII, flux_quad_GxIV
 
 include("split_fluxes.jl")
-# include("split_fluxes_cuda.jl")
+include("split_fluxes_cuda.jl")
 # export flux_Gxp, flux_Gxn, flux_Gyp, flux_Gyn, flux_Gx, flux_Gy
 
 include("state_update.jl")
 # export func_delta, state_update, primitive_to_conserved, conserved_vector_Ubar
 
 include("wall_fluxes.jl")
-# include("wall_fluxes_cuda.jl")
+include("wall_fluxes_cuda.jl")
 # export wall_dGx_pos, wall_dGx_neg, wall_dGy_neg
 
 end
